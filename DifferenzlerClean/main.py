61523d9dcf9fbdc1c11129655ad3ba3f2267d91d
pleasure2cu <adi_hoff@yahoo.de>
2019-12-17 20:11:35 +0100

    test rest of dropout ideas

--- DifferenzlerClean/main.py
+++ DifferenzlerClean/main.py
@@ -29,7 +29,7 @@ if fit_window % parallel_rounds != 0:
 
 
 def main():
-    for discount, dropout in zip([32, 32], [0.1, 0.2]):
+    for discount, dropout in zip([32, 32, 32, 0, 0], [0.3, 0.4, 0.5, 0.2, 0.4]):
         pred_model_funcs = [prediction_resnet]
         strat_model_funcs = [hand_crafted_features_hinton]
         name_bases = ["hinton_net_{}_discount_{}_dropout_player".format(discount, int(dropout*100))]
